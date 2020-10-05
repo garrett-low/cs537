23c9e015d166fa0541683ed350cea1db2f24825a
garrett-low <usernamegl@live.com>
2020-10-04 22:11:24 -0500

    P2A: implemented pipes; working on background execution

--- p2a/wish.c
+++ p2a/wish.c
@@ -22,6 +22,7 @@
 
 #define INPUT_LENGTH_MAX 128
 #define GENERIC_ERROR "An error has occurred\n"
+#define MAX_BACKGROUND_PROC 20
 
 // debug preprocessor directives
 #ifdef DEBUG
@@ -30,18 +31,30 @@
 #define debugPrintf(...) ((void)0)
 #endif
 
-static void printWords(char **cmdArg, char **cmdArgPostAnglePipe,
-                       bool *hasOutputRedir, bool *hasInputRedir, bool *hasPipe,
-                       bool *isBackground);
+// typedef struct pidTableStruct
+// {
+//   int pid[MAX_BACKGROUND_PROC];
+//   int head;
+//   int tail;
+//   int size;
+// } pidTable;
 
 static int readCmd();
 static int parseCmd(char *cmd, char **cmdArg, char **cmdArgPostAnglePipe,
                     bool *hasOutputRedir, bool *hasInputRedir, bool *hasPipe,
                     bool *isBackground);
-static void handleExit();
+static void handleExit(int* bgPids);
 static void handlePwd();
 static void handleCd(char *path);
 static void errorAndEndProcess();
+// static char isEmpty(pidTable bgPids);
+// static void enqueue();
+// static void dequeue();
+
+// debug
+static void debugParse(char **cmdArg, char **cmdArgPostAnglePipe,
+                       bool hasOutputRedir, bool hasInputRedir, bool hasPipe,
+                       bool isBackground);
 
 int main(int argc, char *argv[])
 {
@@ -51,6 +64,7 @@ int main(int argc, char *argv[])
   }
 
   int historyLine = 0;
+  int bgPids[MAX_BACKGROUND_PROC] = {-1};
   while (1)
   {
     historyLine++;
@@ -78,7 +92,7 @@ int main(int argc, char *argv[])
     // built-in commands
     if (strcmp(cmdArg[0], "exit") == 0)
     {
-      handleExit();
+      handleExit(bgPids);
     }
     else if (strcmp(cmdArg[0], "pwd") == 0)
     {
@@ -96,37 +110,93 @@ int main(int argc, char *argv[])
       // child
       if (pid == 0)
       {
-        if (hasOutputRedir == true)
+        if (hasPipe)
         {
-          char *path = cmdArgPostAnglePipe[0];
-          int retVal;
-          if ((retVal = open(path, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU)) == -1)
+          int pipes[2], pidPipe;
+          if (pipe(pipes) == -1)
           {
+            debugPrintf("1\n");
             errorAndEndProcess();
           }
 
-          if (dup2(retVal, 1) == -1)
+          pidPipe = fork();
+          if (pidPipe == 0)
           {
-            errorAndEndProcess();
-          }
-        }
-        else if (hasInputRedir == true)
-        {
-          char *path = cmdArgPostAnglePipe[0];
-          int retVal;
-          if ((retVal = open(path, O_RDONLY)) == -1)
+            if (close(pipes[0]) == -1)
             {
+              debugPrintf("2\n");
               errorAndEndProcess();
             }
-          if (dup2(retVal, 0) == -1)
+
+            if (dup2(pipes[1], STDOUT_FILENO) == -1)
+            {
+              debugPrintf("3\n");
+              errorAndEndProcess();
+            }
+
+            if (execvp(cmdArg[0], cmdArg) == -1)
+            {
+              debugPrintf("4\n");
+              errorAndEndProcess();
+            }
+          }
+          pidPipe = fork();
+          if (pidPipe == 0)
+          {
+            if (close(pipes[1]) == -1)
+            {
+              debugPrintf("5\n");
+              errorAndEndProcess();
+            }
+
+            if (dup2(pipes[0], STDIN_FILENO) == -1)
+            {
+              debugPrintf("6\n");
+              errorAndEndProcess();
+            }
+
+            if (execvp(cmdArgPostAnglePipe[0], cmdArgPostAnglePipe) == -1)
+            {
+              debugPrintf("7\n");
+              errorAndEndProcess();
+            }
+          }
+          else
+          {
+            waitpid(-1, NULL, 0);
+            exit(0);
+          }
+        }
+        else
+        {
+          if (hasOutputRedir)
+          {
+            char *path = cmdArgPostAnglePipe[0];
+
+            int retVal;
+            if ((retVal =
+                     open(path, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU)) == -1)
+              errorAndEndProcess();
+
+            if (dup2(retVal, 1) == -1)
+              errorAndEndProcess();
+          }
+          else if (hasInputRedir)
+          {
+            char *path = cmdArgPostAnglePipe[0];
+
+            int retVal;
+            if ((retVal = open(path, O_RDONLY)) == -1)
+              errorAndEndProcess();
+
+            if (dup2(retVal, STDOUT_FILENO) == -1)
+              errorAndEndProcess();
+          }
+
+          if (execvp(cmdArg[0], cmdArg) == -1)
           {
             errorAndEndProcess();
           }
-        }
-
-        if (execvp(cmdArg[0], cmdArg) == -1)
-        {
-          errorAndEndProcess();
         }
       }
       // parent
@@ -134,20 +204,29 @@ int main(int argc, char *argv[])
       {
         if (!isBackground)
         {
-          waitpid(-1, NULL, 0);
+          waitpid(pid, NULL, 0);
         }
         else
         {
-          /* code */
-        }
-      }
-    }
+          for (int i = 0; i < MAX_BACKGROUND_PROC; i++)
+          {
+            if (bgPids[i] == -1)
+            {
+              bgPids[i] = pid;
+            }
+          }
 
-    if (hasOutputRedir || hasInputRedir || hasPipe)
-    {
-      for (int i = 0; cmdArgPostAnglePipe[i] != NULL; i++)
-      {
-        free(cmdArgPostAnglePipe[i]);
+          for (int i = 0; i < MAX_BACKGROUND_PROC; i++)
+          {
+            if (bgPids[i] != -1)
+            {
+              if (waitpid(bgPids[i], NULL, WNOHANG) == bgPids[i])
+              {
+                bgPids[i] = -1;
+              }
+            }
+          }
+        }
       }
     }
   }
@@ -256,9 +335,7 @@ static int parseCmd(char *cmd, char **cmdArg, char **cmdArgPostAnglePipe,
   while ((cmdArg[i] = strtok_r(NULL, " \t", &savePtr)) != NULL)
   {
     if (cmdArg[i] == NULL)
-    {
       continue;
-    }
 
     // assume redirection and pipeline never used together for this prj
     if (cmdArg[i][0] == '>')
@@ -294,19 +371,13 @@ static int parseCmd(char *cmd, char **cmdArg, char **cmdArgPostAnglePipe,
 
     for (i = 0; cmdArg[anglePipeArgIndex + i + 1] != NULL; i++)
     {
-      char *src = cmdArg[anglePipeArgIndex + i + 1];
-      cmdArgPostAnglePipe[i] = malloc(strlen(src) + 1);
-      if (cmdArgPostAnglePipe[i] == NULL)
-      {
-        write(STDERR_FILENO, GENERIC_ERROR, strlen(GENERIC_ERROR));
-        return -1;
-      }
-      strcpy(cmdArgPostAnglePipe[i], src);
+      cmdArgPostAnglePipe[i] = cmdArg[anglePipeArgIndex + i + 1];
     }
     cmdArgPostAnglePipe[i] = NULL; // set last argv to NULL
 
-    // we expect exactly one word after pipe or redirector
-    if (cmdArgPostAnglePipe[0] == NULL || cmdArgPostAnglePipe[1] != NULL)
+    // we expect exactly one word after redirector
+    if ((*hasOutputRedir || *hasInputRedir) &&
+        (cmdArgPostAnglePipe[0] == NULL || cmdArgPostAnglePipe[1] != NULL))
     {
       write(STDERR_FILENO, GENERIC_ERROR, strlen(GENERIC_ERROR));
       return -1;
@@ -316,8 +387,15 @@ static int parseCmd(char *cmd, char **cmdArg, char **cmdArgPostAnglePipe,
   return 0;
 }
 
-static void handleExit()
+static void handleExit(int* bgPids)
 {
+  for (int i = 0; i < MAX_BACKGROUND_PROC; i++)
+  {
+    if (bgPids[i] != -1)
+    {
+      kill(bgPids[i]);
+    }
+  }
   exit(0);
 }
 
@@ -357,14 +435,14 @@ static void errorAndEndProcess()
   exit(1);
 }
 
-static void printWords(char **cmdArg, char **cmdArgPostAnglePipe,
-                       bool *hasOutputRedir, bool *hasInputRedir, bool *hasPipe,
-                       bool *isBackground)
+static void debugParse(char **cmdArg, char **cmdArgPostAnglePipe,
+                       bool hasOutputRedir, bool hasInputRedir, bool hasPipe,
+                       bool isBackground)
 {
-  debugPrintf("%s - \">\" output redir\n", &hasOutputRedir ? "true" : "false");
-  debugPrintf("%s - \"<\" input redir\n", &hasInputRedir ? "true" : "false");
-  debugPrintf("%s - \"|\" pipeline\n", &hasPipe ? "true" : "false");
-  debugPrintf("%s - \"&\" bg exec\n", &isBackground ? "true" : "false");
+  debugPrintf("%s - \">\" output redir\n", hasOutputRedir ? "true" : "false");
+  debugPrintf("%s - \"<\" input redir\n", hasInputRedir ? "true" : "false");
+  debugPrintf("%s - \"|\" pipeline\n", hasPipe ? "true" : "false");
+  debugPrintf("%s - \"&\" bg exec\n", isBackground ? "true" : "false");
 
   int i = 0;
   while (cmdArg[i] != NULL)
@@ -385,3 +463,71 @@ static void printWords(char **cmdArg, char **cmdArgPostAnglePipe,
     debugPrintf("\"%s\" [%p] - arg%i\n", cmdArgPostAnglePipe[i], &cmdArgPostAnglePipe[i], i);
   }
 }
+
+// /**
+//  * Returns 1 if the set is currently empty, 0 otherwise
+//  */
+// char isEmpty(pidTable bgPids)
+// {
+//   return (bgPids.head == -1) || (bgPids.tail == -1);
+// }
+
+// /**
+//  * Add to the back of the queue by inserting after tail node
+//  */
+// void enqueue(pidTable bgPids, int pid)
+// {
+//   if (bgPids.size >= MAX_BACKGROUND_PROC)
+//   {
+//     write(STDERR_FILENO, GENERIC_ERROR, strlen(GENERIC_ERROR));
+//     return;
+//   }
+
+//   if (isEmpty(bgPids))
+//   { // set is currently empty; head and tail same
+//     bgPids.tail++;
+//     bgPids.head++;
+//     // debugPrintSet(set, 0);
+//   }
+//   else if (bgPids.tail + 1 < MAX_BACKGROUND_PROC)
+//   { // do not wrap around
+//     bgPids.tail++;
+//   }
+//   else
+//   { // wrap around
+//     bgPids.tail = 0;
+//   }
+
+//   bgPids.pid[bgPids.tail] = pid;
+//   bgPids.size++;
+// }
+
+// /**
+//  * Remove from the front of the queue by removing the head node
+//  */
+// void dequeue(pidTable bgPids)
+// {
+//   if (isEmpty(bgPids))
+//   {
+//     write(STDERR_FILENO, GENERIC_ERROR, strlen(GENERIC_ERROR));
+//     return;
+//   }
+
+//   bgPids.pid[bgPids.head] = 0;
+
+//   if (bgPids.head == bgPids.tail)
+//   { // make queue empty
+//     bgPids.head = -1;
+//     bgPids.tail = -1;
+//   }
+//   else if (bgPids.head + 1 < MAX_BACKGROUND_PROC)
+//   { // do not wrap around
+//     bgPids.head++;
+//   }
+//   else
+//   { // wrap around
+//     bgPids.head = 0;
+//   }
+
+//   bgPids.size--;
+// }
