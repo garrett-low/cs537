a2d62e1430ec785b79d74b89432dc245e9633e18
glow <usernamegl@live.com>
2020-10-25 16:12:25 -0500

    Create q2_fork.c

---
+++ mt1/q2_fork.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int value = 5;
+
+int main(int argc, char* argv[]) {
+  pid_t pid;
+  pid = fork();
+  if (pid == 0) {
+    value = value + 15;
+  } else if (pid > 0) {
+    wait(NULL);
+    printf("PARENT : value = %d\n", value);  // LINE A
+    exit(0);
+  }
+}
