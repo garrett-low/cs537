000e42a0ea1018d7958179e0553c5cd36a538cd0
garrett-low <usernamegl@live.com>
2020-12-01 06:15:50 -0600

    P5: 12/25

--- p5/xv6_fsck.c
+++ p5/xv6_fsck.c
@@ -58,7 +58,8 @@ struct dinode {
 // Directory is a file containing a sequence of dirent structures.
 #define DIRSIZ 14
 
-// P5 - My constants
+// P5 - My globals/constants
+void *fsPtr;
 // Debug preprocessor directives
 #ifdef DEBUG
 #define debugPrintf(...) printf(__VA_ARGS__)
@@ -72,6 +73,9 @@ struct dinode {
 #define ERROR1_BAD_INODE "ERROR: bad inode.\n"
 #define ERROR2_BAD_DIRECT_DATA "ERROR: bad direct address in inode.\n"
 #define ERROR2_BAD_INDIRECT_DATA "ERROR: bad indirect address in inode.\n"
+
+void test1(uint i, short type);
+void test2(uint i, struct dinode *);
 
 int main(int argc, char *argv[]) {
   debugPrintf("constants: IPB %ld, BPB %d\n", IPB, BPB);
@@ -97,12 +101,14 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  void *fsPtr = mmap(NULL, fsStat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  // Read in the fs.img into memory
+  fsPtr = mmap(NULL, fsStat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   if (fsPtr == MAP_FAILED) {
     fprintf(stderr, "Failed to map memory to store FS file.\n");
     exit(1);
   }
 
+  // Superblock info
   struct superblock *sBlock;
   sBlock = (struct superblock *)(fsPtr + BSIZE);
   debugPrintf("sBlock: size %d, # data blocks %d, # inodes %d \n", sBlock->size,
@@ -110,41 +116,15 @@ int main(int argc, char *argv[]) {
 
   // Traverse inodes - start at second block
   struct dinode *dinodePtr = (struct dinode *)(fsPtr + (2 * BSIZE));
-  for (int i = 0; i < sBlock->ninodes; i++, dinodePtr++) {
-    short type = dinodePtr->type;
-
+  for (uint i = 0; i < sBlock->ninodes; i++, dinodePtr++) {
     // Test 1 - each inode is either unallocated or one of the valid types
-    if (type != 0 && type != T_FILE && type != T_DIR && type != T_DEV) {
-      debugPrintf("inode %d: type %d\n", i, type);
-      fprintf(stderr, ERROR1_BAD_INODE);
-      exit(1);
-    }
+    test1(i, dinodePtr->type);
 
     // Test 2 - for inuse inodes, points to valid datablock address
-    // TODO: Badindir2
-    if (type == T_FILE) {
-      debugPrintf("file inode %d: ", i);
-      for (int j = 0; j < NDIRECT + 1; j++) {
-        uint dblockNum = dinodePtr->addrs[j];
-        debugPrintf("dblockNum %d, ", dblockNum);
-        if (!(dblockNum == 0 ||
-              ((dblockNum >= 29) && (dblockNum < 995 + 28)))) {
-          // debugPrintf("inode %d: dblockNum: %d \n", i, dblockNum);
-          if (j < NDIRECT) {  // Direct blocks
-            fprintf(stderr, ERROR2_BAD_DIRECT_DATA);
-          } else {  // Indirect blocks
-            fprintf(stderr, ERROR2_BAD_INDIRECT_DATA);
-          }
+    test2(i, dinodePtr);
 
-          exit(1);
-        }
-      }
-      debugPrintf("\n");
-    }
-
-    // Traverse indirect blocks for invalid datablock address
-    uint indirectBlockNum = dinodePtr->addrs[NDIRECT + 1];
-    uint *indirectAddr = (uint *indirectAddr) (indirectBlockNum * BSIZE);
+    // Test 3 - root dir exists, inode num 1, parent is itself
+    
   }
 
   // figure out bitmap
@@ -152,4 +132,55 @@ int main(int argc, char *argv[]) {
   // do rest of P5
 
   return 0;
+}
+
+void test1(uint i, short type) {
+  if (!(type == 0 || type == T_FILE || type == T_DIR || type == T_DEV)) {
+    debugPrintf("ERROR: inode %d, type %d\n", i, type);
+    fprintf(stderr, ERROR1_BAD_INODE);
+    exit(1);
+  }
+}
+
+void test2(uint i, struct dinode *dinodePtr) {
+  if (dinodePtr->type != T_FILE) {
+    return;
+  }
+  
+  // Direct block number invalid
+  debugPrintf("file inode %d: ", i);
+  for (int j = 0; j < NDIRECT; j++) {
+    uint dblockNum = dinodePtr->addrs[j];
+    debugPrintf("dblockNum %d, ", dblockNum);
+    if (!(dblockNum == 0 || ((dblockNum >= 29) && (dblockNum < 995 + 28)))) {
+      debugPrintf("\nERROR: inode %d: dblockNum: %d\n", i, dblockNum);
+      fprintf(stderr, ERROR2_BAD_DIRECT_DATA);
+      exit(1);
+    }
+  }
+  debugPrintf("\n");
+
+  // Indirect block number invalid
+  uint indirectBlockNum = dinodePtr->addrs[NDIRECT];
+  if (!(indirectBlockNum == 0 ||
+        ((indirectBlockNum >= 29) && (indirectBlockNum < 995 + 28)))) {
+    debugPrintf("ERROR: inode %d: indirectBlockNum %d \n", i, indirectBlockNum);
+    fprintf(stderr, ERROR2_BAD_INDIRECT_DATA);
+    exit(1);
+  }
+  if (indirectBlockNum == 0) {
+    return;
+  }
+  
+  // Traverse indirect blocks for invalid datablock address
+  uint *indirectAddr = (uint *)(fsPtr + (indirectBlockNum * BSIZE));
+  for (int j = 0; j < 128; j++, indirectAddr++) {
+    uint dblockNum = *indirectAddr;
+    if (!(dblockNum == 0 || ((dblockNum >= 29) && (dblockNum < 995 + 28)))) {
+      debugPrintf("inode %d: indirectBlockNum %d, dblockNum: %d \n", i,
+                  indirectBlockNum, dblockNum);
+      fprintf(stderr, ERROR2_BAD_INDIRECT_DATA);
+      exit(1);
+    }
+  }
 }
